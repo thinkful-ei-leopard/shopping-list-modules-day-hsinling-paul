832fbbd520da42bddddc94f234fca61af6a4a20d
wavinginspace <paulbaisley@gmail.com>
2020-01-13 15:54:55 -0500

    exercise 3

--- scripts/item.js
+++ scripts/item.js
@@ -1,5 +1,24 @@
 
-export default {};
+function validateName(name) {
+  if (!name) {
+    throw new TypeError('Name must not be blank');
+  }
+} 
+
+function create (name) {
+  const item = {
+    id: cuid(),
+    name: name,
+    checked: false
+  }; 
+  return item;
+}
+
+
+export default {
+  validateName,
+  create
+};
 
 import item from './item.js';
 
